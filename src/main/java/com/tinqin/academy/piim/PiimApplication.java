52f3518ebb1e4ccbc2e1fadb86403aa42f019ef6
a.gerganov <gerganovalexander@gmail.com>
2023-06-09 13:49:03 +0300

    Initial commit - generate Spring Boot App.

---
+++ src/main/java/com/tinqin/academy/piim/PiimApplication.java
@@ -0,0 +1,13 @@
+package com.tinqin.academy.piim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PiimApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PiimApplication.class, args);
+	}
+
+}
