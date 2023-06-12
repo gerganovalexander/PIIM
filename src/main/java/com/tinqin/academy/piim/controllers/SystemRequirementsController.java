d24fa741aedc8617f277278acb2150d8699f245d
a.gerganov <gerganovalexander@gmail.com>
2023-06-12 10:53:59 +0300

    Fixed endpoint

--- src/main/java/com/tinqin/academy/piim/controllers/SystemRequirementsController.java
+++ src/main/java/com/tinqin/academy/piim/controllers/SystemRequirementsController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/system_requirements")
+@RequestMapping("api/system_requirements")
 @RequiredArgsConstructor
 public class SystemRequirementsController {
 
