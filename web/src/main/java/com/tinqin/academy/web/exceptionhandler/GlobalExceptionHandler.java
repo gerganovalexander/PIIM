51cde31bb3faa126579dc23ec7e52668f48febb8
d.tenchev <tenchev.dimitar@gmail.com>
2023-06-13 12:02:31 +0300

    Organized dependencies to corresponding modules

--- web/src/main/java/com/tinqin/academy/web/exceptionhandler/GlobalExceptionHandler.java
+++ web/src/main/java/com/tinqin/academy/web/exceptionhandler/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
-package com.tinqin.academy.business.exceptions;
+package com.tinqin.academy.web.exceptionhandler;
 
+import com.tinqin.academy.business.exceptions.DuplicateEntityException;
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
