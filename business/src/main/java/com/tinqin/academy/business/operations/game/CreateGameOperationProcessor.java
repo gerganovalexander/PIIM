712b6f1e3938e2a7af9f6ecdbff57cf9cb784c99
d.tenchev <tenchev.dimitar@gmail.com>
2023-06-19 10:43:35 +0300

    Removed unused code

--- business/src/main/java/com/tinqin/academy/business/operations/game/CreateGameOperationProcessor.java
+++ business/src/main/java/com/tinqin/academy/business/operations/game/CreateGameOperationProcessor.java
@@ -6,7 +6,6 @@ import com.tinqin.academy.api.game.create.CreateGameResult;
 import com.tinqin.academy.business.exceptions.DuplicateEntityException;
 import com.tinqin.academy.business.exceptions.EmptyEntityException;
 import com.tinqin.academy.data.models.Game;
-import com.tinqin.academy.data.repositories.CategoryRepository;
 import com.tinqin.academy.data.repositories.GameRepository;
 import com.tinqin.academy.ext.steam.interactors.SteamApiInteractor;
 import lombok.RequiredArgsConstructor;
@@ -18,7 +17,6 @@ import org.springframework.stereotype.Service;
 public class CreateGameOperationProcessor implements CreateGameOperation {
 
     private final GameRepository gameRepository;
-    private final CategoryRepository categoryRepository;
     private final ConversionService conversionService;
     private final SteamApiInteractor steamApiInteractor;
 
