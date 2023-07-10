db99baef49bddb29508f43aafea08d24f905b24a
r.georgiev <postfraction@gmail.com>
2023-07-10 11:49:35 +0300

    Added BaseController and handleOperation methods to ReviewController

--- rest/src/main/java/com/tinqin/academy/piim/rest/controllers/ReviewController.java
+++ rest/src/main/java/com/tinqin/academy/piim/rest/controllers/ReviewController.java
@@ -2,33 +2,27 @@ package com.tinqin.academy.piim.rest.controllers;
 
 import com.tinqin.academy.piim.api.review.create.CreateReviewInput;
 import com.tinqin.academy.piim.api.review.create.CreateReviewOperation;
-import com.tinqin.academy.piim.api.review.create.CreateReviewResult;
 import com.tinqin.academy.piim.api.review.delete.DeleteReviewInput;
 import com.tinqin.academy.piim.api.review.delete.DeleteReviewOperation;
-import com.tinqin.academy.piim.api.review.delete.DeleteReviewResult;
 import com.tinqin.academy.piim.api.review.existsbyid.ExistsByIdReviewInput;
 import com.tinqin.academy.piim.api.review.existsbyid.ExistsByIdReviewOperation;
-import com.tinqin.academy.piim.api.review.existsbyid.ExistsByIdReviewResult;
 import com.tinqin.academy.piim.api.review.getall.GetAllReviewsInput;
 import com.tinqin.academy.piim.api.review.getall.GetAllReviewsOperation;
-import com.tinqin.academy.piim.api.review.getall.GetAllReviewsResult;
 import com.tinqin.academy.piim.api.review.getbyid.GetByIdReviewInput;
 import com.tinqin.academy.piim.api.review.getbyid.GetByIdReviewOperation;
-import com.tinqin.academy.piim.api.review.getbyid.GetByIdReviewResult;
 import com.tinqin.academy.piim.api.review.getreviewsbygameid.GetReviewsByGameIdInput;
 import com.tinqin.academy.piim.api.review.getreviewsbygameid.GetReviewsByGameIdOperation;
-import com.tinqin.academy.piim.api.review.getreviewsbygameid.GetReviewsByGameIdResult;
 import com.tinqin.academy.piim.api.review.update.UpdateReviewInput;
 import com.tinqin.academy.piim.api.review.update.UpdateReviewOperation;
-import com.tinqin.academy.piim.api.review.update.UpdateReviewResult;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/reviews")
 @RequiredArgsConstructor
-public class ReviewController {
+public class ReviewController extends BaseController {
 
     private final CreateReviewOperation createReviewOperation;
     private final UpdateReviewOperation updateReviewOperation;
@@ -39,40 +33,41 @@ public class ReviewController {
     private final ExistsByIdReviewOperation existsByIdReviewOperation;
 
     @GetMapping("/{id}")
-    public GetByIdReviewResult getById(@PathVariable @Valid long id) {
-        return getByIdReviewOperation.process(
-                GetByIdReviewInput.builder().id(id).build());
+    public ResponseEntity<?> getById(@PathVariable @Valid long id) {
+        return handleOperation(getByIdReviewOperation.process(
+                GetByIdReviewInput.builder().id(id).build()));
     }
 
     @GetMapping
-    public GetAllReviewsResult getAll() {
-        return getAllReviewsOperation.process(new GetAllReviewsInput());
+    public ResponseEntity<?> getAll() {
+        return handleOperation(getAllReviewsOperation.process(new GetAllReviewsInput()));
     }
 
     @GetMapping(params = "gameId")
-    public GetReviewsByGameIdResult getReviewsByGameId(@RequestParam Long gameId) {
-        return getReviewsByGameIdOperation.process(new GetReviewsByGameIdInput(gameId));
+    public ResponseEntity<?> getReviewsByGameId(@RequestParam Long gameId) {
+        return handleOperation(getReviewsByGameIdOperation.process(new GetReviewsByGameIdInput(gameId)));
     }
 
     @PostMapping
-    public CreateReviewResult create(@RequestBody @Valid CreateReviewInput review) {
-        return createReviewOperation.process(review);
+    public ResponseEntity<?> create(@RequestBody @Valid CreateReviewInput review) {
+        return handleOperation(createReviewOperation.process(review));
     }
 
     @PutMapping("/{id}")
-    public UpdateReviewResult update(@PathVariable long id, @RequestBody @Valid UpdateReviewInput review) {
+    public ResponseEntity<?> update(@PathVariable long id, @RequestBody @Valid UpdateReviewInput review) {
         review.setId(id);
-        return updateReviewOperation.process(review);
+        return handleOperation(updateReviewOperation.process(review));
     }
 
     @DeleteMapping("/{id}")
-    public DeleteReviewResult delete(@PathVariable long id) {
-        return deleteReviewOperation.process(DeleteReviewInput.builder().id(id).build());
+    public ResponseEntity<?> delete(@PathVariable long id) {
+        return handleOperation(
+                deleteReviewOperation.process(DeleteReviewInput.builder().id(id).build()));
     }
 
     @GetMapping("/{id}/exists")
-    public ExistsByIdReviewResult checkIfReviewExistsById(@PathVariable Long id) {
-        return existsByIdReviewOperation.process(
-                ExistsByIdReviewInput.builder().id(id).build());
+    public ResponseEntity<?> checkIfReviewExistsById(@PathVariable Long id) {
+        return handleOperation(existsByIdReviewOperation.process(
+                ExistsByIdReviewInput.builder().id(id).build()));
     }
 }
